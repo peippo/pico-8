cfe98ddc056f46d9e1e4d7a90821a033579efb0a
Janne Peippo <peippo@gmail.com>
2022-08-13 13:56:37 +0300

    Add snow flakes

--- xmas/src/0.init.lua
+++ xmas/src/0.init.lua
@@ -2,10 +2,22 @@
 -- by peippo
 
 function _init()
+    flakes = {}
+    flake_colors = {5, 6, 7}
 end
    
 function _draw()
+    cls(1)
+    
+    add_flake();
+
+    for flake in all(flakes) do
+        flake:draw()
+    end
 end
 
 function _update60()
+    for flake in all(flakes) do
+        flake:update()
+    end
 end
--- xmas/src/1.flakes.lua
+++ xmas/src/1.flakes.lua
@@ -1,30 +1,3 @@
-pico-8 cartridge // http://www.pico-8.com
-version 16
-__lua__
--- xmas is coming
--- by peippo
-
-function _init()
-    flakes = {}
-    flake_colors = {5, 6, 7}
-end
-   
-function _draw()
-    cls(1)
-    
-    add_flake();
-
-    for flake in all(flakes) do
-        flake:draw()
-    end
-end
-
-function _update60()
-    for flake in all(flakes) do
-        flake:update()
-    end
-end
--->8
 -- snow flakes
 
 function add_flake()
@@ -46,4 +19,3 @@ function add_flake()
         end
     })
 end
-
